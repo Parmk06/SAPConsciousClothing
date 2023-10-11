1652c800391b2949556a58c3c318856a5e59e705
Parmk06 <parminderkaur723@gmail.com>
2023-10-11 07:23:55 -0400

    connection

---
+++ admin_area/index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard</title>
+    <!-- bootstrap css link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+
+        <!-- CSS file -->
+        <link rel="stylesheet" href="../style.css">
+
+
+</head>
+<body>
+
+<!-- navbar -->
+<div class="container-fluid p-0">
+    <!-- First child -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-info">
+        <div class="container-fluid">
+
+        <img src="../images/SAP_logo.png" alt="" class="logo">
+
+        <nav class="navbar navbar-expand-lg">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a href="" class="nav-link"><B><h5>Welcome SAP</h5></B></a>
+                </li>
+            </ul>
+
+
+        </nav>
+
+        </div>
+    </nav>
+
+    <!-- Second Child -->
+    <div class="bg-light">
+        <h3 class="text-center p-2">Manage Details</h3>
+    </div>
+
+    <!-- Third Child -->
+    <div class="row">
+        <div class="col-md-12 bg-secondary p-1 d-flex align-items-center">
+            <div class="p-5"> 
+                <!-- Could have written px-5, but p-5 looks better -->
+                <a href="#"><img src="../images/admin-logo.jpg" alt="" class="admin_image"></a>
+                <p class="text-light text-center">Admin SAP</p>
+            </div>
+
+            <div class="button text-center">
+                <button class="my-3"><a href="" class="nav-link text-light bg-info my-1">Insert Products</a></button>
+                <button><a href="" class="nav-link text-light bg-info my-1">View Products</a></button>
+                <button><a href="" class="nav-link text-light bg-info my-1">Insert Categories</a></button>
+                <button><a href="" class="nav-link text-light bg-info my-1">View Categories</a></button>
+                <button><a href="" class="nav-link text-light bg-info my-1">All Orders</a></button>
+                <button><a href="" class="nav-link text-light bg-info my-1">All Payments</a></button>
+                <button><a href="" class="nav-link text-light bg-info my-1">List Users</a></button>
+                <button><a href="" class="nav-link text-light bg-info my-1">Logout</a></button> 
+            </div>
+
+        </div>
+    </div>
+
+
+    <!-- Last Child -->
+
+<!-- LIKE EDEMAMMA -->
+
+<div class="bg-info p-3 text-right footer">
+  <p>© SAP - All Rights Reserved.</p>
+</div>
+
+
+
+</div>
+    
+
+<!-- bootstrap js link -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script> 
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,254 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SAP Conscious Clothing</title>
+    <!-- bootstrap CSS link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+    <!-- font awesome link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+
+    <!-- CSS Files -->
+    <link rel="stylesheet" href="style.css">
+    <style>
+      /* Write the codes over here in case it does not work at Style.css as of catches */
+    </style>
+
+</head>
+<body>
+    <!-- navbar -->
+    <div class="container-fluid p-0">
+        <!-- fist child -->
+        <nav class="navbar navbar-expand-lg bg-info">
+  <div class="container-fluid">
+    <img src="./images/SAP_logo.png" alt="" class="logo">
+    
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Products</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Register</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><i class="fa-solid fa-cart-shopping"></i><sup>1</sup></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Total Price: $20</a>
+        </li>
+        
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-light" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+
+
+
+<!-- Second Child -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
+
+<ul class="navbar-nav me-auto">
+
+<li class="nav-item">
+  <a class="nav-link" href="#">Welcome guest</a>
+</li>
+<li class="nav-item">
+  <a class="nav-link" href="#">Login</a>
+</li>
+</ul>
+</nav>
+
+
+
+
+<!-- Third Child -->
+<div class="bg-light">
+  <h3 class="text-center">SAP Conscious Clothing</h3>
+  <p class="text-center"> Experience the Cultures Through Our Exquisite Attire Collection</p>
+</div>
+
+
+
+
+<!-- Forth Child -->
+
+<div class="row">
+
+
+
+
+    <!-- Side Nav -->
+<div class="col-md-2 bg-secondary p-0">
+     <ul class="navbar-nav me-auto text-center">  
+
+     <li class="nav-item bg-info">
+      <a href="#" class="nav-link text-light"><h4>Categories</h4></a>
+     </li>
+
+     <li class="nav-item">
+      <a href="#" class="nav-link text-light">Choli</a>
+     </li>
+
+     <li class="nav-item">
+      <a href="#" class="nav-link text-light">Salwar</a>
+     </li>
+
+      <li class="nav-item">
+      <a href="#" class="nav-link text-light">Kurti Plazo</a>
+     </li>
+
+     <li class="nav-item">
+      <a href="#" class="nav-link text-light">Sari</a>
+     </li>
+
+
+     </ul>
+</div>
+
+
+    <!-- Products -->
+<div class="col-md-10">
+    <div class="row">
+
+    <div class="col-md-4 mb-2">                
+
+    <div class="card">
+  <img src="./images/royal-choli.jpeg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to cart</a>
+    <a href="#" class="btn btn-secondary">View more</a>
+
+  </div>
+</div>
+
+    </div> 
+
+    <div class="col-md-4 mb-2">
+
+    <div class="card">
+  <img src="./images/Sblackandwhite-choli.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to cart</a>
+    <a href="#" class="btn btn-secondary">View more</a>    
+  </div>
+</div>
+
+    </div>  
+    
+    <div class="col-md-4 mb-2">
+
+    <div class="card">
+  <img src="./images/Spink-orange-choli.jpeg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to cart</a>
+    <a href="#" class="btn btn-secondary">View more</a>    
+  </div>
+</div>
+    </div>  
+
+
+
+    <div class="col-md-4 mb-2">
+
+    <div class="card">
+  <img src="./images/red-orange.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to cart</a>
+    <a href="#" class="btn btn-secondary">View more</a>    
+  </div>
+</div>
+    </div>  
+
+
+
+
+    <div class="col-md-4 mb-2">
+
+    <div class="card">
+  <img src="./images/colorful.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to cart</a>
+    <a href="#" class="btn btn-secondary">View more</a>    
+  </div>
+</div>
+    </div>    
+
+
+
+    <div class="col-md-4 mb-2">
+
+    <div class="card">
+  <img src="./images/red-suit.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to cart</a>
+    <a href="#" class="btn btn-secondary">View more</a>    
+  </div>
+</div>
+    </div>  
+
+
+
+
+    </div>
+  </div>
+
+
+
+
+</div>
+
+
+
+
+
+
+
+<!-- Last Child -->
+
+<!-- LIKE EDEMAMMA -->
+
+<div class="bg-info p-3 text-right">
+  <p>© SAP - All Rights Reserved.</p>
+</div>
+
+
+
+    </div>
+
+<!-- bootstrap JS link -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script> 
+
+</body>
+</html>
