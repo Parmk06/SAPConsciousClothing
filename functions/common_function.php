2b935b4a523624aed04047988cc676c6ba70468c
Parmk06 <parminderkaur723@gmail.com>
2023-10-15 07:07:23 -0400

    function added in main

--- functions/common_function.php
+++ functions/common_function.php
@@ -2,8 +2,37 @@
 //including connect file
 
 include('../includes/connect.php');
+include('functions/common_function.php');
 
+//getting products
+function getproducts(){
+    $select_query="Select * from `products` order by rand() LIMIT 0,9"; //0 to 9 is limit of products on one page
+    $result_query=mysqli_query($con,$select_query);
+    // $row=mysqli_fetch_assoc($result_query);
+    // echo $row['product_title'];
+    while($row=mysqli_fetch_assoc($result_query)){
 
+      $product_id=$row['product_id'];
+      $product_title=$row['product_title'];
+      $product_description=$row['product_description'];
+      // $product_keywords=$row['product_keywords'];
+      $category_id=$row['category_id'];
+      $product_image=$row['product_image'];
+      $product_price=$row['product_price'];
 
+      echo "<div class='col-md-4 mb-2'>
+            <div class='card'>
+                        <img src='./admin_area/product_images/$product_image' class='card-img-top' alt='$product_title'>
+                        <div class='card-body'>
+                        <h5 class='card-title'>$product_title</h5>
+                        <p class='card-text'>$product_description</p>
+                        <a href='#' class='btn btn-info'>Add to cart</a>
+                        <a href='#' class='btn btn-secondary'>View more</a>
+                        </div>
+        </div>
+    </div>;
+   
+    }
+  }
 ?>
-
+    
